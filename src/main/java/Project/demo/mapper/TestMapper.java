ea5860983ac284389bda1291b86d9eedc5612e1f
Human3321 <sgh7308@gmail.com>
2022-12-12 15:20:16 +0900

    API_SERVER

---
+++ src/main/java/Project/demo/mapper/TestMapper.java
@@ -0,0 +1,15 @@
+package Project.demo.mapper;
+
+import Project.demo.DTO.TestDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Mapper
+
+public interface TestMapper {
+    List<TestDTO> getUserList();
+    List<TestDTO> SearchNumber(String phone);
+}
+
+
